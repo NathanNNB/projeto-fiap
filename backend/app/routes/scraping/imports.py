66ad8488e743c1a97a497596e2140899f93d4331
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-06 13:29:54 -0300

    Add subop at imports

--- backend/app/routes/scraping/imports.py
+++ backend/app/routes/scraping/imports.py
@@ -1,11 +1,39 @@
 from flask import Blueprint, jsonify, request
 from flasgger import swag_from
 from app.routes.routes import URL
+from app.services.scraping_service import scrape_page
+
 IMPORTS_PARAM = 'opcao=opt_05' 
 
 imports = Blueprint('imports', __name__)
 
-@imports.route('/api/imports/imports', methods=['GET'])
+@imports.route('/api/imports/', methods=['GET'])
 def getImports():
-    return jsonify({"message": "imports"})
+    importsURL = URL
+    params = IMPORTS_PARAM
+
+    year = request.args.get('year', default='', type=str)
+    classification = request.args.get('classification', default='', type=str)
+
+
+    if year:
+        if 1970 < int(year) > 2023:
+            return jsonify({"message": "Invalid Year, use a value between 1970 and 2023"})
+        yearParameter = f"?ano={year}"
+        importsURL = f"{importsURL}{yearParameter}"
+        params = f"{params}{year}"
+
+    if classification: 
+        classificationParameter = f"?subopcao={classification}"
+        importsURL= f"{importsURL}{classificationParameter}"
+        params = f"{params}{classification}"
+
+
+    importsURL = f"{importsURL}&{IMPORTS_PARAM}" 
+    
+    data = scrape_page(importsURL, params)
+    if "error" in data:
+        return data
+    print(data)
+    return jsonify({"message": str(data)})
 
--- frontend/src/services/imports.js
+++ frontend/src/services/imports.js
@@ -2,16 +2,9 @@ import { API } from "./api";
 
 const API_URL = API;
 
-export const fetchImportsIndex = async (query) => {
-  const response = await API_URL.get('/imports', {
-    params: { query },
-  });
-  return response.data;
-};
-
 export const fetchImportsByYear = async (query) => {
     const response = await API_URL.get('/imports', {
-      params: { query },
+      params:  query ,
     });
     return response.data;
   };
