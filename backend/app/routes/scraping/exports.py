03e0f73c6aef1e63a9c449decd39f0bcce14bd4a
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-06 13:47:13 -0300

    fix endpoint

--- backend/app/routes/scraping/exports.py
+++ backend/app/routes/scraping/exports.py
@@ -7,31 +7,30 @@ EXPORTS_PARAM = 'opcao=opt_06'
 
 exports = Blueprint('exports', __name__)
 
-@exports.route('/api/exports/exports', methods=['GET'])
+@exports.route('/api/exports/', methods=['GET'])
 def getExports():
     exportsURL = URL
     params = EXPORTS_PARAM
 
     year = request.args.get('year', default='', type=str)
     classification = request.args.get('classification', default='', type=str)
+    if year or classification:
+        exportsURL = f"{exportsURL}?"
 
     if year:
         if 1970 < int(year) > 2023:
             return jsonify({"message": "Invalid Year, use a value between 1970 and 2023"})
-        yearParameter = f"?ano={year}"
+        yearParameter = f"ano={year}&"
         exportsURL = f"{exportsURL}{yearParameter}"
         params = f"{params}{year}"
 
     if classification: 
-        classificationParameter = f"?subopcao={classification}"
+        classificationParameter = f"subopcao={classification}&"
         exportsURL= f"{exportsURL}{classificationParameter}"
         params = f"{params}{classification}"
 
-    exportsURL = f"{exportsURL}&{EXPORTS_PARAM}" 
-    
+    exportsURL = f"{exportsURL}{EXPORTS_PARAM}" 
     data = scrape_page(exportsURL, params)
     if "error" in data:
         return data
-    print(data)
     return jsonify({"message": str(data)})
-
