f284f2799c296bd77bded4057591ddc94a51f013
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-07 00:24:12 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,17 +1,17 @@
 import os
 from flask import Flask, request, redirect
 from app.routes import create_app
+from flask_cors import CORS
 
 app = create_app()
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.before_request
 def enforce_https_in_production():
-    if os.getenv("FLASK_ENV") == "production" and request.url.startswith("http://"):
+    if os.getenv("FLASK_ENV") == "production" and not request.is_secure:
         url = request.url.replace("http://", "https://", 1)
-        app.logger.info(f"Redirecionando: {request.url} -> {url}")
         return redirect(url, code=301)
 
 if __name__ == "__main__":
-    debug_mode = os.getenv("FLASK_ENV") != "production"
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=debug_mode)
+    port = int(os.getenv("PORT", 5000))  # Porta definida pela Render
+    app.run(host="0.0.0.0", port=port)
