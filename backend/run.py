ce5e47b9e7bda4ee0d77a36591309a1bf4fc4100
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-07 00:34:58 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -6,12 +6,6 @@ from flask_cors import CORS
 app = create_app()
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.before_request
-def enforce_https_in_production():
-    if os.getenv("FLASK_ENV") == "production" and not request.is_secure:
-        url = request.url.replace("http://", "https://", 1)
-        return redirect(url, code=301)
-
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))  # Porta definida pela Render
     app.run(host="0.0.0.0", port=port)
