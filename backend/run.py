5aa73bc1c7f67cecef995e4224e8ee4091f910c6
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-07 00:04:19 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -6,11 +6,12 @@ app = create_app()
 
 @app.before_request
 def enforce_https_in_production():
-    # Verifica se está em produção
     if os.getenv("FLASK_ENV") == "production" and request.url.startswith("http://"):
         url = request.url.replace("http://", "https://", 1)
+        app.logger.info(f"Redirecionando: {request.url} -> {url}")
         return redirect(url, code=301)
 
 if __name__ == "__main__":
+    debug_mode = os.getenv("FLASK_ENV") != "production"
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=debug_mode)
