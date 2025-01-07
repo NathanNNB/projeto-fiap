ecd7e28c93f93d5f8ae809a46c4dd0d3304df457
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-06 23:55:12 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,15 @@
 import os
+from flask import Flask, request, redirect
 from app.routes import create_app
 
 app = create_app()
+
+@app.before_request
+def enforce_https_in_production():
+    # Verifica se está em produção
+    if os.getenv("FLASK_ENV") == "production" and request.url.startswith("http://"):
+        url = request.url.replace("http://", "https://", 1)
+        return redirect(url, code=301)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
