5860e43cfd255c62e00a8ad553d0c0b99ca756ff
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-07 22:35:06 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -4,21 +4,18 @@ from app.routes import create_app
 from flask_cors import CORS
 
 app = create_app()
+
 CORS(app, resources={r"/api/*": {"origins": "https://projeto-fiap-coral.vercel.app"}})
 
 @app.before_request
 def enforce_https_in_production():
-    url = request.url.replace("http://", "https://", 1)
-    app.logger.info(f"Requisição recebida: {request.url}")
-    app.logger.info(f"Requisição final: {request.url}")
-    app.logger.info("getEnv FLASK ENV:" + os.getenv("FLASK_ENV"))
-    app.logger.info(f"request.is_secure: {request.is_secure}")
-    return redirect(url, code=301)
-
-@app.route("/api/processing")
-def processing():
-    return {"message": "Requisição processada com sucesso!"}
+    if os.getenv("FLASK_ENV") == "production":
+        url = request.url.replace("http://", "https://", 1)
+        app.logger.info(f"Redirecionando: {request.url} -> {url}")
+        return redirect(url, code=301)
 
 if __name__ == "__main__":
+    print(os.getenv("FLASK_ENV"))
+    debug_mode = os.getenv("FLASK_ENV") != "production"
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=debug_mode)
