39fb19ea919d6a4c30c24faa4cd523a665b2edb1
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-07 21:50:38 -0300

    test new run.py

--- backend/run.py
+++ backend/run.py
@@ -6,6 +6,17 @@ from flask_cors import CORS
 app = create_app()
 CORS(app, resources={r"/api/*": {"origins": "https://projeto-fiap-coral.vercel.app"}})
 
+@app.before_request
+def enforce_https_in_production():
+    if os.getenv("FLASK_ENV") == "production" and not request.is_secure:
+        url = request.url.replace("http://", "https://", 1)
+        return redirect(url, code=301)
+    app.logger.info(f"Requisição recebida: {request.url}")
+    
+@app.route("/api/processing")
+def processing():
+    return {"message": "Requisição processada com sucesso!"}
+
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Porta definida pela Render
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
