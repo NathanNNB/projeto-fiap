ea79d63c86e137f620e32687e76cf2ce36c2b020
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-07 22:02:29 -0300

    add logger

--- backend/run.py
+++ backend/run.py
@@ -10,9 +10,12 @@ CORS(app, resources={r"/api/*": {"origins": "https://projeto-fiap-coral.vercel.a
 def enforce_https_in_production():
     if os.getenv("FLASK_ENV") == "production" and not request.is_secure:
         url = request.url.replace("http://", "https://", 1)
+        app.logger.info(f"Requisição recebida: {request.url}")
+        app.logger.info(f"Requisição final: {request.url}")
+        app.logger.info(f"getEnv FLASK ENV: {os.getenv("FLASK_ENV")}")
+        app.logger.info(f"request.is_secure: {request.is_secure}")
         return redirect(url, code=301)
-    app.logger.info(f"Requisição recebida: {request.url}")
-    
+
 @app.route("/api/processing")
 def processing():
     return {"message": "Requisição processada com sucesso!"}
