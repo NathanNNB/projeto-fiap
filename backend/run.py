1b4eabeb218f97560ab5c832e8bf36d6ef5f822a
Nathan Novais Borges <nathan-borges2011@live.com>
2025-01-07 23:01:43 -0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -4,18 +4,8 @@ from app.routes import create_app
 from flask_cors import CORS
 
 app = create_app()
-
-CORS(app, resources={r"/api/*": {"origins": "https://projeto-fiap-coral.vercel.app"}})
-
-@app.before_request
-def enforce_https_in_production():
-    if os.getenv("FLASK_ENV") == "production":
-        url = request.url.replace("http://", "https://", 1)
-        app.logger.info(f"Redirecionando: {request.url} -> {url}")
-        return redirect(url, code=301)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 if __name__ == "__main__":
-    print(os.getenv("FLASK_ENV"))
-    debug_mode = os.getenv("FLASK_ENV") != "production"
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=debug_mode)
+    port = int(os.getenv("PORT", 5000))  # Porta definida pela Render
+    app.run(host="0.0.0.0", port=port)
